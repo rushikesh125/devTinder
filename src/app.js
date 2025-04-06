f9b992946dc94ba48dc3823c8bd300d67ff757fc
jackop <lolits2626@gmail.com>
2025-04-06 18:34:05 +0530

    routes added & tested

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const usersData = require("../users.json")
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.json({
     msg: "Hello From Server",
   });
 });
+
+app.get("/users",(req,res)=>{
+    res.json(usersData)
+})
+app.post("/users/:id",(req,res)=>{
+    console.log(req.params)
+    res.json(usersData)
+})
 
 app.listen(3000, () => {
   console.log("Server started on Port 3000");
